fe4dc2886ab6c877cccfcf6b0f0301b9b62ce07e
Cristian Rincon <crincon@grupodot.com>
2021-04-05 19:35:20 -0500

    se generan adecuaciones para conectar a deta

---
+++ app/blog/routers/blog.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, Response, status
+from sqlalchemy.orm import Session
+
+from blog import database, oauth2, schemas
+from blog.repository import blog
+
+router = APIRouter(tags=['blogs'], prefix='/blog')
+get_db = database.get_db
+
+
+@router.get('', response_model=List[schemas.BlogResponse])
+def get_all_posts(db: Session = Depends(get_db),
+                  current_user: schemas.User = Depends(
+                      oauth2.get_current_user)):
+    return blog.get_all(db)
+
+
+@router.post('', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog,
+           db: Session = Depends(get_db),
+           user_id: int = 1,
+           current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.create(request, db, user_id)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int,
+                db: Session = Depends(get_db),
+                current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.delete(id, db)
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_post(id,
+                request: schemas.Blog,
+                db: Session = Depends(get_db),
+                current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.update(id, request, db)
+
+
+@router.get('/{id}',
+            status_code=status.HTTP_200_OK,
+            response_model=schemas.BlogResponse)
+def get_post(id: int,
+             response: Response,
+             db: Session = Depends(get_db),
+             current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.get_one(id, response, db)
---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from blog import models
+from blog.database import engine
+from blog.routers import authentication, blog, user
+
+models.Base.metadata.create_all(engine)
+app = FastAPI()
+
+app.include_router(blog.router)
+app.include_router(user.router)
+app.include_router(authentication.router)
+
+
+@app.get('/')
+def index():
+    return RedirectResponse('/docs')
